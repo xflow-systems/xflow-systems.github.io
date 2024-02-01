2f4c89134db1da5afccb479438a40a3d61825d0e
Andreas Stocker <xflow-systems@proton.me>
2024-02-01 18:56:32 +0100

    app: add pull vs push based

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,24 +5,36 @@ function App() {
         <div className="bg-zinc-100 p-10 font-bold">
             XFlow Systems
         </div>
-        <div className="p-10 text-3xl font-bold">
-            What is XFlow?
-        </div>
 
-        <div className="px-10">
-            <div className="py-4">XFlow is a <b>Database</b> combined with a <b>Fullstack Framework</b></div>
-        </div>
+        <div className="p-10">
+            <div className="py-10 text-3xl font-bold">
+                What is XFlow?
+            </div>
 
-        <div className="p-10" dangerouslySetInnerHTML={{__html: Components}}></div>
-        
-        <div className="p-10 text-3xl font-bold">
-            How is XFlow Different?
-        </div>
-        <div className="p-10 text-2xl">
-            XFlow is <b>Push Based</b>
-        </div>
-        <div className="px-10 text-xl">
-            XFlow is <b>Push Based</b>
+            <div className="px-10">
+                <div className="py-4">XFlow is a <b>Database</b> combined with a <b>Fullstack Framework</b></div>
+            </div>
+
+            <div className="py-10" dangerouslySetInnerHTML={{ __html: Components }}></div>
+
+            <div className="py-10 text-3xl">
+                The XFlow Database is <b>Push Based</b>
+            </div>
+            <div className="p-4 text-2xl text-bold">
+                Push Based vs Pull Based Databases
+            </div>
+            <div className="p-4 text-xl text-bold">
+                <div className="py-4">
+                    Traditional pull based databases work with one-off queries. They send a a query, they get a result.
+                    If something might have changed, they need to know when to send another query.
+                </div>
+                <div className="py-4">
+                    Pull based databases don't need to guess if something has changed. They subscribe to a query and continously
+                    get results as changes occur.
+                </div>
+
+
+            </div>
         </div>
     </div >
 }
