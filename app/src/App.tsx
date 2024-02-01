f30e150ada8f154febe12953e24e1635a8eefb30
Andreas Stocker <xflow-systems@proton.me>
2024-02-01 17:37:48 +0100

    init react vite app, add tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+    return <div className="mx-auto max-w-6xl font-bold text-xl">
+        <div className="bg-zinc-100 p-10">
+            XFlow Systems
+        </div>
+        <div className="p-10 text-3xl">
+            What is XFlow?
+        </div>
+        <div className="p-10">
+            XFlow has various components:
+        </div>
+
+        <div className="p-10 text-3xl">
+            Why a new System?
+        </div>
+    </div>
+}
+
+export default App
