e1c754600a21370148046f59adce38f10fd4c6c0
Andreas Stocker <xflow-systems@proton.me>
2024-04-30 16:38:29 +0200

    app: button active class

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,8 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+
+function buttonActiveClass({ isActive }: { isActive: boolean }) {
+    return isActive ? 'text-zinc-500 cursor-default' : 'underline';
+}
 
 export default function App() {
     return <div className="mx-auto max-w-7xl text-xl">
@@ -6,13 +10,16 @@ export default function App() {
             Check Out XFlow Alpha Version 0.1.0 (Live In-Browser Code Editor)!
         </a>
 
-        <div className="flex">
-            <div className="w-1/4 p-10 border-l-2 border-zinc-200">
+        <div className="flex flex-col lg:flex-row">
+            <div className="lg:w-1/4 p-10 border-l-2 border-b-2 lg:border-b-0 border-zinc-200">
                 <div className="pb-10 font-bold text-2xl">Posts</div>
-                <div className="pb-10 underline"><Link to={"/"}>What is XFlow?</Link></div>
-                <div className="pb-10 underline"><Link to={"/query-engine"}>The Query Engine</Link></div>
+                <div className="pb-10"><NavLink className={buttonActiveClass} to={"/"}>What is XFlow?</NavLink></div>
+                <div className="pb-10"><NavLink className={buttonActiveClass} to={"/query-engine"}>The Query Engine</NavLink></div>
             </div>
-            <div className="w-3/4 border-x-2 border-zinc-200 pb-20">
+            <div className="lg:w-3/4 border-x-2 border-zinc-200 pb-20">
+                <div className="">
+                    
+                </div>
                 <Outlet />
             </div>
         </div>
