7641cfe918bab2a1642afe6c2faaf51c6a25b5c0
Andreas Stocker <xflow-systems@proton.me>
2024-04-30 15:44:07 +0200

    add react router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import WhatIs from "./Posts/WhatIs";
+
+const router = createHashRouter([{
+    path: "/",
+    element: <App />,
+    children: [{
+        path: "/",
+        element: <WhatIs />,
+    }],
+    errorElement: <div>Wrong Route</div>,
+}]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
