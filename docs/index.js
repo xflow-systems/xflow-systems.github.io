b85377828e18770074ba20dcc0f12e812b5c6840
Andreas Stocker <xflow-systems@proton.me>
2024-10-08 21:19:03 +0200

    add font

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,8 @@
 import { html, render } from 'https://unpkg.com/htm/preact/standalone.module.js'
 
-const App = (name) => html`hello!! ${name}<b>dude</b>`;
+const App = (name) => html`<div class="main-container">
+    hello!! ${name}<b>dude</b>
+</div>`;
 
 render(App("andy"), document.querySelector("#root"));
 
