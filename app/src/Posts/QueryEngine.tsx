2d7ef0a6f1e046e02e1dcee4cfd75519b5cd4aa1
Andreas Stocker <xflow-systems@proton.me>
2024-10-08 21:04:02 +0200

    remove vite/tailwind project

--- app/src/Posts/QueryEngine.tsx
+++
@@ -1,7 +0,0 @@
-export default function QueryEngine() {
-    return <div className="p-10">
-        <div className="py-10 text-3xl font-bold">
-            The Query Engine
-        </div>
-    </div>;
-}
